8eef5a30b00620198debb72debd1df45cb35ec66
SaiKumar297 <21pa1a12c0@vishnu.edu.in>
2024-06-30 20:14:54 +0530

    Initial Commit

---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+import WorkIm1 from '../assets/projects/e-commerce.png'
+import WorkIm2 from '../assets/projects/social-demo.png'
+import WorkImg4 from '../assets/projects/Learning.jpg'
+import WorkIm3 from '../assets/projects/Food-Donation.png'
+
+export const data=[
+    {
+        id:1,
+        name:"E-Commerce_Demo",
+        image:WorkIm1,
+        github:"https://github.com/SaiKumar297/E-Commerce_React.git",
+    },
+    {
+        id:2,
+        name:"Social-Media_Demo",
+        image:WorkIm2,
+        github:"https://github.com/SaiKumar297/social-demo1.git",
+
+    },
+    {
+        id:3,
+        name:"Learning 4.0",
+        image:WorkImg4,
+        github:"https://github.com/CharanGude/GDSC",
+      
+    },
+   
+   
+
+
+]
